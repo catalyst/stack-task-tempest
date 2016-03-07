b8d0f035679c6016a50d8c6ce3bc835dfa02c8e6
Dale Smith <dale@catalyst-eu.net>
2016-03-07 14:58:18 +0000

    Invite user implemented.

--- stacktask_tempest_plugin/plugin.py
+++ stacktask_tempest_plugin/plugin.py
@@ -1,28 +1,16 @@
-# Copyright 2015
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
-
 import os
 
 from tempest import config
 from tempest.test_discover import plugins
+from stacktask_tempest_plugin import config as config_share
 
-from stacktask_tempest_plugin import config as project_config
 
+class MyPlugin(plugins.TempestPlugin):
+    def get_opt_lists(self):
+        return [(
+            config_share.stacktask_group.name,
+            config_share.StacktaskGroup)]
 
-class StacktaskTempestPlugin(plugins.TempestPlugin):
     def load_tests(self):
         base_path = os.path.split(os.path.dirname(
             os.path.abspath(__file__)))[0]
@@ -31,7 +19,11 @@ class StacktaskTempestPlugin(plugins.TempestPlugin):
         return full_test_dir, base_path
 
     def register_opts(self, conf):
-        pass
-
-    def get_opt_lists(self):
-        pass
+        config.register_opt_group(
+            conf,
+            config_share.service_available_group,
+            config_share.ServiceAvailableGroup)
+        config.register_opt_group(
+            conf,
+            config_share.stacktask_group,
+            config_share.StacktaskGroup)
