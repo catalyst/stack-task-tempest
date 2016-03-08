5c16fbd3935b051edf57a7cfad0610f09b4df548
Dale Smith <dale@catalyst-eu.net>
2016-03-08 11:55:13 +0000

    Import more tempest.lib, remove NYI tests.

--- stacktask_tempest_plugin/stacktask_client.py
+++ stacktask_tempest_plugin/stacktask_client.py
@@ -3,10 +3,10 @@
 from oslo_serialization import jsonutils as json
 from six.moves.urllib import parse as urllib
 
-from tempest.common import service_client
+from tempest.lib.common import rest_client
 
 
-class StacktaskClient(service_client.ServiceClient):
+class StacktaskClient(rest_client.RestClient):
 
     def user_list(self, params=None):
         """Lists all users within the tenant."""
@@ -18,7 +18,7 @@ class StacktaskClient(service_client.ServiceClient):
         resp, body = self.get(uri)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body)
+        return rest_client.ResponseBody(resp, body)
 
     def user_invite(self, username, email, roles):
         """ Invite a user to the tenant. """
@@ -33,7 +33,7 @@ class StacktaskClient(service_client.ServiceClient):
         resp, body = self.post(uri, body=post_body)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body)
+        return rest_client.ResponseBody(resp, body)
 
     def revoke_user(self, user_id):
         """ Revoke a user from the tenant
@@ -48,7 +48,7 @@ class StacktaskClient(service_client.ServiceClient):
             # note: this breaks. stacktask returns a string, not json.
             return
         self.expected_success(200, resp.status)
-        return service_client.ResponseBody(resp, None)
+        return rest_client.ResponseBody(resp, None)
 
     def get_tokens(self, json_data={}, filters={}):
         """ Returns dict of tokens matching the provided filters """
@@ -59,7 +59,7 @@ class StacktaskClient(service_client.ServiceClient):
         resp, body = self.get(uri, headers=filter_json)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body)
+        return rest_client.ResponseBody(resp, body)
 
     def token_submit(self, token_id, json_data={}):
         """ Submits a given token, along with optional data """
@@ -69,4 +69,4 @@ class StacktaskClient(service_client.ServiceClient):
         resp, body = self.post(uri, post_body)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body)
+        return rest_client.ResponseBody(resp, body)
